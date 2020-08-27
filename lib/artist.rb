2a80d7e52b5b8126e8c9ab730592f1079e55752d
Chaim Bryski <luzybry94@gmail.com>
2020-08-27 22:12:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   
-  extend Memorable, Findable
+  extend Memorable::ClassMethods, Findable
   include Memorable::InstanceMethods, Paramable
   
   attr_accessor :name
