6dcbd53da95c5dd62b39529f96e6b7be0e34e49a
Chaim Bryski <luzybry94@gmail.com>
2020-08-27 21:42:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Song
   
-  extend Memorable 
+  extend Memorable
+  include 
   
   attr_accessor :name
   attr_reader :artist
@@ -13,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
   def self.all
     @@songs
